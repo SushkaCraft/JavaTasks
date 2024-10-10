17917f7a1410cff9a41172354ff0cec5e2bd789b
SushkaCraft <102476271+SushkaCraft@users.noreply.github.com>
2024-10-10 13:43:09 +0300

    Update lab2.java Fix Z4

--- lab2.java
+++ lab2.java
@@ -211,25 +211,71 @@ public class Main {
         int price = 0;
 
         switch (age) {
-            case 0, 1, 2, 3, 4, 5, 6:
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
                 System.out.println("Билет бесплатный.");
                 break;
-            case 7, 8, 9, 10, 11:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
                 price = 150;
                 System.out.println("Стоимость билета: " + price + " рублей.");
                 break;
-            case 12, 13, 14, 15, 16, 17:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
                 price = 200;
                 System.out.println("Стоимость билета: " + price + " рублей.");
                 break;
-            case 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
                 price = 250;
                 System.out.println("Стоимость билета: " + price + " рублей.");
                 break;
             default:
                 if (age >= 50) {
                     price = 250;
-                    double discountedPrice = price * 0.95; // 5% скидка
+                    double discountedPrice = price * 0.95;
                     System.out.println("Стоимость билета с пенсионной скидкой: " + discountedPrice + " рублей.");
                 } else {
                     System.out.println("Некорректный возраст.");
