94bf232b81e9500de1a7df6d10784449ad074d95
SushkaCraft <102476271+SushkaCraft@users.noreply.github.com>
2024-10-25 19:34:26 +0300

    Update Product.java

--- lab3/Product.java
+++ lab3/Product.java
@@ -1,19 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Product
-{
-    public int ID;
-    public String Name;
-    public String Description;
-    public double Price;
-    public boolean Availability;
-    public enum Category {};
-    public String Brand;
-    public int Sale;
-    public String Date;
-    public int Warranty;
-    
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean available;
+    private Category category;
+    private String brand;
+    private double rating;
+    private int discount;
+    private String arrivalDate;
+    private int warranty;
+
+    public enum Category {
+        ELECTRONICS,
+        FURNITURE,
+        CLOTHING,
+        BEAUTY,
+        TOYS
+    }
+
+    public Product(int id, String name, String description, double price, boolean available, Category category, String brand, int discount, double rating, String arrivalDate, int warranty) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.available = available;
+        this.category = category;
+        this.brand = brand;
+        this.discount = discount;
+        this.rating = rating;
+        this.arrivalDate = arrivalDate;
+        this.warranty = warranty;
+    }
 }
