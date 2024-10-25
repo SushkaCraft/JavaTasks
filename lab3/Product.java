5ed6c9e1338899fabcb5d66515567d15caf0c708
SushkaCraft <102476271+SushkaCraft@users.noreply.github.com>
2024-10-25 15:00:14 +0300

    Add Folder For Third Lab

---
+++ lab3/Product.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Product
+{
+    public int ID;
+    public String Name;
+    public String Description;
+    public double Price;
+    public boolean Availability;
+    public enum Category {};
+    public String Brand;
+    public int Sale;
+    public String Date;
+    public int Warranty;
+    
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
